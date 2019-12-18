b0982184ac1ae12ac1f94c119f0aa3c51532c892
Philip Ross <dev@baseballlover723.com>
2019-12-18 15:00:01 -0800

    added method to get what champion you played

--- content.js
+++ content.js
@@ -1,7 +1,26 @@
+"use strict";
 console.log("On op.gg");
-$(".GameItemWrap .Stats").each(function() {
-  var statsDiv = $(this);
-  statsDiv.append('<div>Hello Test</div>');
+$(".GameItemWrap").each((_, gameDiv) => {
+  gameDiv = $(gameDiv);
+  const yourStatsDiv = gameDiv.find(".Stats");
+  yourStatsDiv.append('<div>Hello Test</div>');
   console.log("adding test");
+  const ownStats = getOwnStats(gameDiv);
+  console.log("own stats", ownStats);
+  return false;
 });
+
+function getOwnStats(gameDiv) {
+  const ownChampion = getOwnChampion(gameDiv);
+  console.log("own Champion", ownChampion);
+  const stats = {};
+
+  return stats;
+}
+
+function getOwnChampion(gameDiv) {
+  const championImage = gameDiv.find(".GameSettingInfo .ChampionImage img");
+  const splitPath = new URL("http:" + championImage.attr("src")).pathname.replace(/\.[^/.]+$/, "").split("/");
+  return splitPath.pop() || splitPath.pop();
+}
 
